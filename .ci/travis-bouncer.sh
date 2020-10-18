fb5351b6c64df617221482ec9015e420d65bf18b
tooomm <tooomm@users.noreply.github.com>
2020-10-19 00:08:32 +0200

    Update travis-bouncer.sh

--- .ci/travis-bouncer.sh
+++ .ci/travis-bouncer.sh
@@ -7,6 +7,7 @@
 # https://medium.com/@manjula.cse/how-to-stop-the-execution-of-travis-pipeline-if-script-exits-with-an-error-f0e5a43206bf
 # echo "No content yet in travis-bouncer.sh"
 
+#print to logs checken, exit code
 if [ -z $(git diff --name-only --diff-filter=d $TRAVIS_COMMIT_RANGE | grep -v "\.md$") ]; then
           echo "Only Markdown files were modified, skipping CI run.";
           exit;
