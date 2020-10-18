82444ffeb1b806a8b0252e38d88078b5a70b7c3b
tooomm <tooomm@users.noreply.github.com>
2020-10-19 00:03:04 +0200

    check if only md files got changed

--- .ci/travis-bouncer.sh
+++ .ci/travis-bouncer.sh
@@ -5,6 +5,11 @@
 # not yet executable
 # look into `travis_terminate` to stop a travis build from continuing to execute following stages
 # https://medium.com/@manjula.cse/how-to-stop-the-execution-of-travis-pipeline-if-script-exits-with-an-error-f0e5a43206bf
-echo "No content yet in travis-bouncer.sh"
+# echo "No content yet in travis-bouncer.sh"
+
+if [ -z $(git diff --name-only --diff-filter=d $TRAVIS_COMMIT_RANGE | grep -v "\.md$") ]; then
+          echo "Only Markdown files were modified, skipping CI run.";
+          exit;
+fi
 
 exit 0
